f64c8b5f3d0c7427a57613e09b3f72404f0386ff
Christian Fries <christian.fries@lst.team>
2022-03-09 12:57:23 +0100

    [RELEASE] 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sebastian Michaelsen',
     'author_email' => 'sebastian@michaelsen.io',
     'state' => 'stable',
-    'version' => '0.1.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
     ],
 ];
